61da386ec56c01768be2bc088f2d53e9f95b5e69
hyunwoo <music2eye.hw@gmail.com>
2017-07-29 08:40:45 +0900

    함수들 구조 변경 및 추가코딩

--- ftl_greedy/custom.c
+++ ftl_greedy/custom.c
@@ -6,44 +6,19 @@ static void set_vsn(UINT32 const lsn, UINT32 const vsn)
 {
   int map_addr;
 
-  if(!is_mapping_table_cached(lsn))
-  {
-  	inc_map_blk_cnt(bank);
-  	if(!is_mapping_table_full(bank))
-  	{
-  		flush_page(bank);	//민지누나가 짜셔야 할 부분
-  	}
-
-  	map_addr = CACHE_MAP_ADDR + caching_table(bank);
-
-  	
-
-  } 
-
+	map_addr = CACHE_MAP_ADDR + find_caching_addr(bank);
   //update metadata
   //????
 
   write_dram_32(map_addr + (lsn % SECTORS_PER_PAGE) * sizeof(UINT32), vsn);
 }
 
+
 static UINT32 get_vsn(UINT32 const lsn)
 {
   int map_addr;
 
-  if(!is_mapping_table_cached(lsn))
-  {
-  	inc_map_blk_cnt(bank);
-  	if(!is_mapping_table_full(bank))
-  	{
-  		flush_page(bank);	//민지누나가 짜셔야 할 부분
-  	}
-
-  	map_addr = CACHE_MAP_ADDR + caching_table(bank);
-
-  	
-
-  }
-
+	map_addr = CACHE_MAP_ADDR + find_caching_addr(bank);
   //update metadata
   //????
 
@@ -51,21 +26,35 @@ static UINT32 get_vsn(UINT32 const lsn)
 }
 
 
-static int is_mapping_table_cached(UINT32 const lsn)
+static UINT32 find_caching_addr(UINT32 const bank)
 {
-	if(mem_search_equ_dram(CACHE_LRU_ADDR, (?????) , (????), (lsn / SECTORS_PER_PAGE) * SECTORS_PER_PAGE))
-	{
-		return true;
-	}
+	UINT32 addr;
 
-	return false;
-}
+	if(!is_mapping_table_cached(lsn))
+  {
+  	inc_map_blk_cnt(bank);
 
+  	if(!is_mapping_table_full(bank))
+  	{
+  		flush_page(bank);	// 정책에 따라 victim page를 nand에 flush하는 함수, 민지누나가 짜셔야 할 부분
+  	}
+  }
 
-static void caching_table(UINT32 const bank)
-{
-	//?????
+  addr = free_page_addr(bank);	// caching mapping table을 관리하는 테이블로부터 free page addr을 반환하는 함수, 민지누나가 짜셔야 할 부분
 
 	//update metadata
 	dec_map_blk_cnt(bank);
+
+	return  addr;
+}
+
+
+static int is_mapping_table_cached(UINT32 const lsn)
+{
+	if(mem_search_equ_dram(CACHE_LRU_ADDR, (?????), (????), (lsn / SECTORS_PER_PAGE) * SECTORS_PER_PAGE))
+	{
+		return true;
+	}
+	
+	return false;
 }
